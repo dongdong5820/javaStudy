807dab2ca9a1b57b1f9a5c83517baf940195a92b
dongdong5820 <407591245@qq.com>
2021-04-26 21:49:54 +0800

    打造一个小超市程序

---
+++ src/letsjava/chapter2/supermarket/Merchandise.java
@@ -0,0 +1,14 @@
+package letsjava.chapter2.supermarket;
+
+/**
+ * @author ranlay.su
+ * @date 2021-04-26 20:53
+ * @desc 商品类
+ */
+public class Merchandise {
+    public String name;
+    public String id;
+    public int count;
+    public double soldPrice;
+    public double purchasePrice;
+}
