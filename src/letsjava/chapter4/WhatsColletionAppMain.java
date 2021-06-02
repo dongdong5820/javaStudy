1b686b002f27bd49b49b6741ba9455d317065d5a
dongdong5820 <407591245@qq.com>
2021-06-02 09:50:39 +0800

    collection中的list

---
+++ src/letsjava/chapter4/WhatsColletionAppMain.java
@@ -0,0 +1,14 @@
+package letsjava.chapter4;
+
+import java.util.Collection;
+
+/**
+ * @author ranlay.su
+ * @date 2021-05-31 21:10
+ * @desc
+ */
+public class WhatsColletionAppMain {
+    public static void main(String[] args) {
+        Collection collection = null;
+    }
+}
