e159a7857b2c7f9ca03eef8518f4c1e97d568511
dongdong5820 <407591245@qq.com>
2021-06-06 12:48:35 +0800

    接口

---
+++ src/letsjava/chapter4/s06/mylist/MyArrayList.java
@@ -0,0 +1,152 @@
+package letsjava.chapter4.s06.mylist;
+
+import java.util.*;
+
+/**
+ * @author ranlay.su
+ * @date 2021-06-05 18:46
+ */
+public class MyArrayList<T> implements List<T> {
+    private Object[] elements;
+    private int curr;
+
+    public MyArrayList() {
+        elements = new Object[16];
+        curr = 0;
+    }
+
+    @Override
+    public int size() {
+        return curr;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return curr == 0;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        for (Object ele : elements) {
+            if (Objects.equals(ele, o)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    // >> TODO 实现 Iterable 接口里定义的 iterator接口
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            int pointer = 0;
+
+            @Override
+            public boolean hasNext() {
+                return pointer < size();
+            }
+
+            @SuppressWarnings("unchecked")
+            @Override
+            public T next() {
+                return (T) elements[pointer++];
+            }
+        };
+    }
+
+    @Override
+    public Object[] toArray() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Object[] toArray(Object[] a) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean add(T t) {
+        return false;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends T> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends T> c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public T get(int index) {
+        return null;
+    }
+
+    @Override
+    public T set(int index, T element) {
+        return null;
+    }
+
+    @Override
+    public void add(int index, T element) {
+
+    }
+
+    @Override
+    public T remove(int index) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public ListIterator<T> listIterator() {
+        return null;
+    }
+
+    @Override
+    public ListIterator<T> listIterator(int index) {
+        return null;
+    }
+
+    @Override
+    public List<T> subList(int fromIndex, int toIndex) {
+        return null;
+    }
+}
