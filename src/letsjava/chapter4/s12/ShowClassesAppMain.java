cee5e1ddfac9ddc99a58426c9ea353f94f52b32d
dongdong5820 <407591245@qq.com>
2021-06-15 10:03:20 +0800

    Java IO 简介

---
+++ src/letsjava/chapter4/s12/ShowClassesAppMain.java
@@ -0,0 +1,26 @@
+package letsjava.chapter4.s12;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.Reader;
+import java.io.Writer;
+
+/**
+ * @author ranlay.su
+ * @date 2021-06-15 9:51
+ */
+public class ShowClassesAppMain {
+    public static void main(String[] args) {
+        // TODO 输入输出字节，最基础的接口
+        InputStream inputStream;
+        OutputStream outputStream;
+
+        // TODO 增加将字节编码为字符的功能，只输入输出字符
+        Reader reader;
+        Writer writer;
+
+        // TODO 当时我花了好久，才让自己分清楚什么是输入什么是输出
+        // TODO 输入输出是站在程序的角度说的，程序从外部设备读取数据，就是输入，就是input，read
+        // TODO 程序写数据到外部设备，就是输出，就是output,writer
+    }
+}
